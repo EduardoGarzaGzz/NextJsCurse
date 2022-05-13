2f02badf9d1d68aaacf84421e823ecd9a8ced032
Eduardo Uriel Garza Gonzalez <EDUARDO.URIEL.GARZA.GONZALEZ@AMWAY.COM>
2022-05-13 16:31:17 -0500

    05/13/2022 End class 111

--- 03-open-jira/components/ui/NewEntry.tsx
+++ 03-open-jira/components/ui/NewEntry.tsx
@@ -1,22 +1,42 @@
-import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined'
-import SaveOutlinedIcon             from '@mui/icons-material/SaveOutlined'
-import { Box, Button, TextField }   from '@mui/material'
-import { FC }                       from 'react'
+import AddCircleOutlineOutlinedIcon  from '@mui/icons-material/AddCircleOutlineOutlined'
+import SaveOutlinedIcon              from '@mui/icons-material/SaveOutlined'
+import { Box, Button, TextField }    from '@mui/material'
+import { ChangeEvent, FC, useState } from 'react'
 
 export const NewEntry: FC = () => {
+	const [ isAdding, setIsAdding ]     = useState( false )
+	const [ inputValue, setInputValue ] = useState( '' )
+	const [ touched, setTouched ]       = useState( false )
+	const onTextFieldChanged            = ( event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement> ) => {
+		setInputValue( event.target.value )
+	}
+	const onSave                        = () => {
+
+	}
 
 	return (
 		<Box sx={ { marginBottom: 2, paddingX: 2 } }>
-			<Button fullWidth variant={ 'outlined' } startIcon={ <AddCircleOutlineOutlinedIcon /> }> Agregar Tarea</Button>
-			<TextField fullWidth placeholder={ 'Nueva entrada' } autoFocus multiline label={ 'Nuevo entrada' }
-					   helperText={ 'Ingrese un valor' } sx={ {
-				marginTop   : 2,
-				marginBottom: 1
-			} } />
-			<Box display={ 'flex' } justifyContent={ 'space-between' }>
-				<Button variant={ 'text' }>Cancelar</Button>
-				<Button variant={ 'outlined' } color={ 'secondary' } endIcon={ <SaveOutlinedIcon /> }>Guardar</Button>
-			</Box>
+			{
+				isAdding
+				? ( <>
+					<TextField fullWidth placeholder={ 'Nueva entrada' } autoFocus multiline label={ 'Nuevo entrada' }
+							   error={ inputValue.length <= 0 && touched }
+							   value={ inputValue }
+							   onChange={ onTextFieldChanged }
+							   onBlur={ () => setTouched( true ) }
+							   helperText={ inputValue.length <= 0 && touched && 'Ingrese un valor' } sx={ {
+						marginTop   : 2,
+						marginBottom: 1
+					} } />
+					<Box display={ 'flex' } justifyContent={ 'space-between' }>
+						<Button onClick={ () => setIsAdding( false ) } variant={ 'text' }>Cancelar</Button>
+						<Button onClick={ onSave } variant={ 'outlined' } color={ 'secondary' }
+								endIcon={ <SaveOutlinedIcon /> }>Guardar</Button>
+					</Box>
+				</> )
+				: ( <Button onClick={ () => setIsAdding( true ) } fullWidth variant={ 'outlined' }
+							startIcon={ <AddCircleOutlineOutlinedIcon /> }> Agregar Tarea </Button> )
+			}
 		</Box>
 	)
 }
